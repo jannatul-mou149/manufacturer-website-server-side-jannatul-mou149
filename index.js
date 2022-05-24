6ae7566871995016a404031ba53fbe80aa5cec95
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-05-24 20:22:49 +0600

    manage product delete api

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ async function run() {
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
+        //delete a product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
         //loading order data for payment
         app.get('/new-order/:id', async (req, res) => {
             const id = req.params.id;
